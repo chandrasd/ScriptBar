914048a05cbb7f822afcb267c8daff0d24e67b1e
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2025-07-02 23:45:20 -0700

    Initial Commit

---
+++ scripter/scripterApp.swift
@@ -0,0 +1,17 @@
+//
+//  scripterApp.swift
+//  scripter
+//
+//  Created by Chandra Dasari on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct scripterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
